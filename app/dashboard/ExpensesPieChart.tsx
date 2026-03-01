234e102bf83ddb72cb6438c7cdc6a84cf22fdb44
S0ffen <whitevolf314@gmail.com>
2026-03-01 11:43:01 +0100

    added chartcomponent and prepared data for it in page.tsx on dashboard main

---
+++ app/dashboard/ExpensesPieChart.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+export default function ExpensesPieChart(Data: any) {
+  console.log("Data in ExpensesPieChart  :", Data);
+  return (
+    <div>
+      <h1>Hi</h1>
+      <p>Welcome to PieChart component</p>
+    </div>
+  );
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation";
 import SignOutButton from "@/app/dashboard/SignOutButton";
 import { getServerSession } from "@/app/src/lib/session";
 import { AddExpenseDialog } from "./AddExpenseDialog";
+import { prisma } from "@/app/src/lib/prisma";
+import ExpensesPieChart from "./ExpensesPieChart";
 
 export default async function DashboardPage() {
   const session = await getServerSession();
@@ -9,6 +11,24 @@ export default async function DashboardPage() {
   if (!session) {
     redirect("/login");
   }
+  const userExpenses = await prisma.expense.findMany({
+    where: { userId: session.user.id },
+    orderBy: { createdAt: "desc" },
+  });
+
+  const userExpensesByCategory = userExpenses.reduce<Record<string, number>>((acc, expense) => {
+    const parsedAmount = Number(expense.amount);
+    if (!acc[expense.category]) {
+      acc[expense.category] = 0;
+    }
+
+    // console.log("this is acc:", acc);
+    // console.log("this is expense:", expense);
+
+    acc[expense.category] += parsedAmount;
+    return acc;
+  }, {});
+  console.log("User expenses by category:", userExpensesByCategory);
 
   return (
     <main className="mx-auto flex min-h-screen w-full max-w-2xl flex-col gap-4 p-6">
@@ -18,6 +38,7 @@ export default async function DashboardPage() {
       <p className="text-sm text-zinc-600">To jest przykladowa strona po zalogowaniu.</p>
 
       <AddExpenseDialog />
+      <ExpensesPieChart data={userExpensesByCategory} />
     </main>
   );
 }
